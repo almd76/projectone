fc8bb5a255fba741a233484e50fe0c3c8891dd39
almd76 <rafael99almd@gmail.com>
2023-09-04 01:51:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
-/* function atravessar(){
-  console.log("olhar para a direita")
-  console.log("olhar para a esquerda")
-  console.log("atravessar")
-}
-
-atravessar() */
 
 function imprimir(i) {
-    //i = 5
     console.log(i)
   }
   
   function multiplicar(a, b){
-    //imprimir(a * b)
     return a * b
   }
   
