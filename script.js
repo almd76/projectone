b1c9f7189be8339744c073e47157aa2332efc39b
almd76 <rafael99almd@gmail.com>
2023-09-04 01:49:25 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+/* function atravessar(){
+  console.log("olhar para a direita")
+  console.log("olhar para a esquerda")
+  console.log("atravessar")
+}
+
+atravessar() */
+
+function imprimir(i) {
+    //i = 5
+    console.log(i)
+  }
+  
+  function multiplicar(a, b){
+    //imprimir(a * b)
+    return a * b
+  }
+  
+  var resultado = multiplicar(3, 5)
+  imprimir(resultado)
